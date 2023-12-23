0b695d126a2c2a9db93a459dc5c6d649db2c2dac
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-23 18:17:15 -0300

    feat: Header component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="bg-primary container py-4">
+      <div>
+        <h1 className="text-xl font-semibold">Jest | RTL | HookForm | Zod</h1>
+      </div>
+    </header>
+  );
+}
