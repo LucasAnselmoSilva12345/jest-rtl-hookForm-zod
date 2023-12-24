d26f8901de1ca593bb7bc8ddd20832d364bbe441
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-24 18:31:31 -0300

    test: Form component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/tests/Form.spec.tsx
+++ src/tests/Form.spec.tsx
@@ -1,8 +1,44 @@
 import { Form } from '../components/Form';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
 describe('Form Component', () => {
   it('should render form', () => {
     render(<Form />);
+  });
+
+  it('should render form on user screen', () => {
+    render(<Form />);
+
+    const inputName = screen.getByRole('textbox', { name: 'Name' });
+    const inputEmail = screen.getByRole('textbox', { name: 'E-mail' });
+    const inputAge = screen.getByRole('spinbutton', { name: 'Age' });
+    const inputSelectGender = screen.getByRole('combobox', {
+      name: 'Select-Gender',
+    });
+    const inputPhotURL = screen.getByRole('textbox', { name: 'Photo-URL' });
+    const inputProfession = screen.getByRole('textbox', { name: 'Profession' });
+    const inputIsStuddent = screen.getByRole('textbox', { name: 'Is-Student' });
+    const inputCity = screen.getByRole('textbox', { name: 'City' });
+    const inputState = screen.getByRole('textbox', { name: 'State' });
+    const inputCountry = screen.getByRole('textbox', { name: 'Country' });
+    const inputPassword = screen.getByLabelText('Password');
+    const inputConfirmPassword = screen.getByLabelText('Confirm-Password');
+    const agreeTerms = screen.getByRole('checkbox', { name: 'Agree-Terms' });
+    const buttonSubmit = screen.getByRole('button', { name: /Send/i });
+
+    expect(inputName).toBeVisible();
+    expect(inputEmail).toBeVisible();
+    expect(inputAge).toBeVisible();
+    expect(inputSelectGender).toBeVisible();
+    expect(inputPhotURL).toBeVisible();
+    expect(inputProfession).toBeVisible();
+    expect(inputIsStuddent).toBeVisible();
+    expect(inputCity).toBeVisible();
+    expect(inputCountry).toBeVisible();
+    expect(inputState).toBeVisible();
+    expect(inputPassword).toBeVisible();
+    expect(inputConfirmPassword).toBeVisible();
+    expect(agreeTerms).toBeVisible();
+    expect(buttonSubmit).toBeVisible();
   });
 });
