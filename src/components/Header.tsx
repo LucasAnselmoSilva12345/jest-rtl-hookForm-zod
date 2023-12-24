dc827cdd0e4183ab43729ca3ad3f2589bfad3a5e
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-24 16:37:12 -0300

    feat: Mode Toggle switch theme component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
+import { ModeToggle } from './mode-toggle';
+
 export function Header() {
   return (
     <header className="bg-primary container py-4">
-      <div>
+      <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">Jest | RTL | HookForm | Zod</h1>
+        <ModeToggle />
       </div>
     </header>
   );
