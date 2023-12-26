452d99ee081f4b9caea0c61a5b72317b3bd817f9
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-26 15:49:55 -0300

    test: Show error message when form was empty and form was filled
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/tests/Form.spec.tsx
+++ src/tests/Form.spec.tsx
@@ -1,5 +1,6 @@
+import userEvent from '@testing-library/user-event';
 import { Form } from '../components/Form';
-import { screen, render } from '@testing-library/react';
+import { screen, render, act, waitFor } from '@testing-library/react';
 
 describe('Form Component', () => {
   it('should render form', () => {
@@ -42,5 +43,134 @@ describe('Form Component', () => {
     expect(inputConfirmPassword).toBeVisible();
     expect(agreeTerms).toBeVisible();
     expect(buttonSubmit).toBeVisible();
+  });
+
+  it('should show error message when fields was empty', async () => {
+    render(<Form />);
+    const buttonSubmit = screen.getByRole('button', { name: /Send/i });
+
+    act(() => {
+      userEvent.click(buttonSubmit);
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('The field name should have at least one character')
+      ).toBeVisible();
+      expect(
+        screen.getByText(
+          'The field last name should have at least one character'
+        )
+      ).toBeVisible();
+      expect(
+        screen.getByText('This field email should be filled')
+      ).toBeVisible();
+      expect(screen.getByText('Please enter a positive number')).toBeVisible();
+      expect(screen.getByText('Please select an option')).toBeVisible();
+      expect(screen.getByText('Please enter a valid URL')).toBeVisible();
+      expect(screen.getByText('Please provide your profession')).toBeVisible();
+      expect(
+        screen.getByText('The field city should have at least one character')
+      ).toBeVisible();
+      expect(
+        screen.getByText('The field state should have at least one character')
+      ).toBeVisible();
+      expect(
+        screen.getByText('The country state should have at least one character')
+      ).toBeVisible();
+      expect(
+        screen.getByText('Password should be at least six characters')
+      ).toBeVisible();
+      expect(screen.getByText('Please accept the terms')).toBeVisible();
+    });
+  });
+
+  it('should type into fields and submit form', async () => {
+    render(<Form />);
+
+    const inputName = screen.getByRole('textbox', { name: 'Name' });
+    const inputLastName = screen.getByRole('textbox', { name: 'Last-Name' });
+    const inputEmail = screen.getByRole('textbox', { name: 'E-mail' });
+    const inputAge = screen.getByRole('spinbutton', { name: 'Age' });
+    const inputSelectGender = screen.getByRole('combobox', {
+      name: 'Select-Gender',
+    });
+    const inputPhotURL = screen.getByRole('textbox', { name: 'Photo-URL' });
+    const inputProfession = screen.getByRole('textbox', { name: 'Profession' });
+    const inputIsStuddent = screen.getByRole('textbox', { name: 'Is-Student' });
+    const inputCity = screen.getByRole('textbox', { name: 'City' });
+    const inputState = screen.getByRole('textbox', { name: 'State' });
+    const inputCountry = screen.getByRole('textbox', { name: 'Country' });
+    const inputPassword = screen.getByLabelText('Password');
+    const inputConfirmPassword = screen.getByLabelText('Confirm-Password');
+    const agreeTerms = screen.getByRole('checkbox', { name: 'Agree-Terms' });
+    const buttonSubmit = screen.getByRole('button', { name: /Send/i });
+
+    act(() => {
+      userEvent.type(inputName, 'Bruce');
+      userEvent.type(inputLastName, 'Wayne');
+      userEvent.type(inputAge, '10');
+      userEvent.type(inputEmail, 'brucewayne@gmail.com');
+      userEvent.type(inputSelectGender, 'Male');
+      userEvent.type(
+        inputPhotURL,
+        'https://unsplash.com/photos/closeup-photo-of-shuriken-on-wood-n-2_KHgeAy0'
+      );
+      userEvent.type(inputIsStuddent, 'yes');
+      userEvent.type(inputProfession, 'Businessperson');
+      userEvent.type(inputCity, 'Los Angeles');
+      userEvent.type(inputState, 'Colorado');
+      userEvent.type(inputCountry, 'United State of America');
+      userEvent.type(inputPassword, 'bruceWayne2023');
+      userEvent.type(inputConfirmPassword, 'bruceWayne2023');
+      userEvent.click(agreeTerms);
+      userEvent.click(buttonSubmit);
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText('The field name should have at least one character')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText(
+          'The field last name should have at least one character'
+        )
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('This field email should be filled')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Please enter a positive number')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Please select an option')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Please enter a valid URL')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Please provide your profession')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('The field city should have at least one character')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('The field state should have at least one character')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText(
+          'The country state should have at least one character'
+        )
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Password should be at least six characters')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('The passwords should be equals')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Please accept the terms')
+      ).not.toBeInTheDocument();
+    });
   });
 });
