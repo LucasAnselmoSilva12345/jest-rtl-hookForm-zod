935b9beafb033a090e24ba72f4a891a17c772a21
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-01-04 12:32:01 -0300

    test: HandleSubmitForm Prop of Home Component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/tests/Home.spec.tsx
+++ src/tests/Home.spec.tsx
@@ -1,8 +1,10 @@
 import { Form } from '../components/Form';
 import { render } from '@testing-library/react';
 
+const handleSubmitForm = jest.fn();
+
 describe('Home Component', () => {
   it('should render form', () => {
-    render(<Form />);
+    render(<Form handleSubmitForm={handleSubmitForm} />);
   });
 });
